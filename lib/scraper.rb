9f43d931d1bfce4163531033aab7bbb75e035f04
Ian Hutson <ih00209@georgiasouthern.edu>
2020-06-09 15:56:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,7 +21,7 @@ class Scraper
   end
   
   def get_courses
- doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+ 
     doc.css(".post").first.css("h2").text
   end
 end
